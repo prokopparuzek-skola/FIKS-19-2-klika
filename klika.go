dd3dba930fbcef50894c457eff584bb30a331652
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-11-10 13:35:49 +0100

    uz to neco vyhazuje, spatne

--- klika.go
+++ klika.go
@@ -24,7 +24,7 @@ type state struct {
 
 func dijkstra(airports *graph, start int) {
 	// pole vrcholů, ukládají se zde dosud spočítané nejkratší cesty
-	var vertex []state = make([]state, len(airports.flights)+airports.K+1)
+	var vertex []state = make([]state, len(airports.flights))
 	for i := range vertex {
 		vertex[i].cost = -1
 		vertex[i].heapIndex = -1
@@ -37,14 +37,15 @@ func dijkstra(airports *graph, start int) {
 	heapAdd(&Overtex, &vertex, start)
 	for len(Overtex) > 1 {
 		v := extractMin(&Overtex, &vertex)
+		//	println(v)
 		if v <= airports.K { // letiště v paktu
 			for i := 0; i < airports.K; i++ {
 				if vertex[i].cost == -1 {
-					vertex[i].cost = vertex[v].cost + airports.X
+					vertex[i].cost = vertex[v].cost + airports.X // cena
 					vertex[i].heapIndex = len(Overtex)
 					heapAdd(&Overtex, &vertex, i)
 				} else if vertex[i].cost > (vertex[v].cost + airports.X) {
-					vertex[i].cost = vertex[v].cost + airports.X
+					vertex[i].cost = vertex[v].cost + airports.X // uprava ceny
 					Overtex[vertex[i].heapIndex].cost = vertex[v].cost + airports.X
 					bubbleUp(&Overtex, &vertex, vertex[i].heapIndex)
 				}
@@ -52,10 +53,21 @@ func dijkstra(airports *graph, start int) {
 		}
 		for _, w := range airports.flights[v] { // ostatní letiště
 			if vertex[w.to].cost == -1 {
-				vertex[w.to].cost = vertex[v].cost + airports.X
+				vertex[w.to].cost = vertex[v].cost + w.cost // cena
 				vertex[w.to].heapIndex = len(Overtex)
 				heapAdd(&Overtex, &vertex, w.to)
+			} else if vertex[w.to].cost > (vertex[v].cost + w.cost) {
+				vertex[w.to].cost = vertex[v].cost + w.cost // uprava ceny
+				Overtex[vertex[w.to].heapIndex].cost = vertex[v].cost + w.cost
+				bubbleUp(&Overtex, &vertex, vertex[w.to].heapIndex)
 			}
+		}
+	}
+	for i := 1; i < len(vertex); i++ {
+		if i < len(vertex)-1 {
+			fmt.Printf("%d ", vertex[i].cost)
+		} else {
+			fmt.Printf("%d", vertex[i].cost)
 		}
 	}
 	return
@@ -73,6 +85,7 @@ func bubbleUp(Overtex *[]top, vertex *[]state, n int) {
 			(*vertex)[n].heapIndex = parent
 			(*vertex)[parent].heapIndex = n
 		}
+		n /= 2
 	}
 }
 
@@ -90,6 +103,7 @@ func bubbleDown(Overtex *[]top, vertex *[]state, n int) {
 			(*vertex)[n].heapIndex = son
 			(*vertex)[son].heapIndex = n
 		}
+		n *= 2
 	}
 }
 
@@ -102,11 +116,12 @@ func heapAdd(Overtex *[]top, vertex *[]state, vrx int) {
 }
 
 func extractMin(Overtex *[]top, vertex *[]state) (vrx int) {
-	swp := (*Overtex)[0]
+	swp := (*Overtex)[1]
 	vrx = swp.vrx
-	(*Overtex)[0] = (*Overtex)[len(*Overtex)-1]
-	bubbleDown(Overtex, vertex, 1)
+	(*Overtex)[1] = (*Overtex)[len(*Overtex)-1]
 	*Overtex = (*Overtex)[:len(*Overtex)-1]
+	bubbleDown(Overtex, vertex, 1)
+	(*vertex)[vrx].heapIndex = -1
 	return
 }
 
@@ -117,7 +132,7 @@ func main() {
 	fmt.Scanf("%d %d %d %d %d", &N, &K, &X, &M, &V)
 	airports.K = K
 	airports.X = X
-	airports.flights = make([][]edge, N)
+	airports.flights = make([][]edge, N+1)
 	for i := 0; i < N; i++ {
 		airports.flights[i] = make([]edge, 0)
 	}
@@ -127,4 +142,5 @@ func main() {
 		airports.flights[from] = append(airports.flights[from], edge{to, cost})
 		airports.flights[to] = append(airports.flights[to], edge{from, cost})
 	}
+	dijkstra(&airports, V)
 }
